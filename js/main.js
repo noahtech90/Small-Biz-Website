4fe122211cab637d19203e26b9c86ea1a1db9f4a
Noah <noahhodge2@gmail.com>
2020-10-05 17:43:40 -0700

    Food Biz Website

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('today')
+selectElement.innerHTML = formatDate;
+
+
+function changeActive() {
+    let allNavItems = document.getElementsByClassName("nav-link");
+    let length = allNavItems.length;
+    for (i = 0; i < length; i++) {
+        
+        allNavItems[i].addEventListener('click', function()  {
+            let current = document.getElementsByClassName(" active")
+        if (current.length >= 1) {
+            current[0].className = current[0].className.replace(" active", "")
+        }
+        
+        this.className += " active";
+            }
+        )
+    }
+   
+}
